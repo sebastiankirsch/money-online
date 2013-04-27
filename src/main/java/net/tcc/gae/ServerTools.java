859a0384582586d7c699ca815410868998e03a79
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-04-27 19:13:07 +0200

    project skeleton

---
+++ src/main/java/net/tcc/gae/ServerTools.java
@@ -0,0 +1,62 @@
+package net.tcc.gae;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+
+public final class ServerTools {
+
+    private static final PersistenceManagerFactory pmfInstance = JDOHelper.getPersistenceManagerFactory();
+
+    public static PersistenceManager getPersistenceManager() {
+        return pmfInstance.getPersistenceManager();
+    }
+
+    public static void close(PersistenceManager persistenceManager) {
+        Transaction tx = persistenceManager.currentTransaction();
+        try {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+        } finally {
+            persistenceManager.close();
+        }
+    }
+
+    public static Transaction startTransaction(PersistenceManager pm) {
+        Transaction tx = pm.currentTransaction();
+        tx.begin();
+        return tx;
+    }
+
+    private static <T> T execute(PersistenceTemplate<T> template, boolean withTransaction) {
+        PersistenceManager persistenceManager = getPersistenceManager();
+        try {
+            Transaction transaction = withTransaction ?
+                    startTransaction(persistenceManager) : null;
+            T result = template.doWithPersistenceManager(persistenceManager);
+            if (withTransaction) transaction.commit();
+            return result;
+        } finally {
+            close(persistenceManager);
+        }
+    }
+
+    public static <T> T executeWithoutTransaction(PersistenceTemplate<T> template) {
+        return execute(template, false);
+    }
+
+    public static <T> T executeWithTransaction(PersistenceTemplate<T> template) {
+        return execute(template, true);
+    }
+
+    private ServerTools() {
+        super();
+    }
+
+    public static interface PersistenceTemplate<T> {
+        public T doWithPersistenceManager(PersistenceManager persistenceManager);
+    }
+
+}
