b4881f96164f39695eaa54f3326034a9c48f8d9e
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-05-04 20:08:55 +0200

    logging added

--- src/main/java/net/tcc/money/online/server/worker/PricesWorker.java
+++ src/main/java/net/tcc/money/online/server/worker/PricesWorker.java
@@ -20,6 +20,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static java.lang.System.currentTimeMillis;
 import static net.tcc.gae.ServerTools.executeWithoutTransaction;
 import static net.tcc.gae.ServerTools.startTransaction;
 
@@ -56,46 +57,14 @@ public class PricesWorker extends HttpServlet {
             @Override
             public Collection<PersistentPrice> doWithPersistenceManager(PersistenceManager persistenceManager) {
                 PersistentPurchase purchase = fetchPurchase(persistenceManager);
-                PersistentShop shop = purchase.getShop();
-                PersistentPrices persistentPrices = fetchPrices(persistenceManager, shop);
-
-                Date purchaseDate = purchase.getPurchaseDate();
-                Transaction tx = startTransaction(persistenceManager);
-                for (PersistentPurchasing purchasing : purchase) {
-                    if (purchasing.getQuantity() == null) {
-                        continue;
-                    }
-                    PersistentArticle article = purchasing.getArticle();
-                    BigDecimal price = purchasing.getPrice().divide(purchasing.getQuantity());
-                    PersistentPrice existingPrice = persistentPrices.getPriceFor(article);
-                    if (existingPrice == null) {
-                        persistentPrices.addPriceFor(article, purchaseDate, price);
-                        if (LOG.isLoggable(Level.INFO)) {
-                            LOG.info("Storing new price for " + shop + "/" + article);
-                        }
-                        continue;
-                    }
-                    boolean priceHasNotChanged = existingPrice.getPrice().equals(price);
-                    boolean existingPriceIsNewerThanNewPrice = existingPrice.getSince().compareTo(purchaseDate) > 0;
-                    if (priceHasNotChanged || existingPriceIsNewerThanNewPrice) {
-                        if (LOG.isLoggable(Level.FINE)) {
-                            LOG.fine("Keeping existing price for " + shop + "/" + article);
-                        }
-                        continue;
-                    }
-                    existingPrice.setSince((Date) purchaseDate.clone());
-                    existingPrice.setPrice(price);
-                    if (LOG.isLoggable(Level.INFO)) {
-                        LOG.info("Updating existing price for " + shop + "/" + article);
-                    }
-                }
-                persistenceManager.makePersistent(persistentPrices);
-                tx.commit();
+                PersistentPrices persistentPrices = fetchPrices(persistenceManager, purchase.getShop());
+                updatePrices(persistenceManager, purchase, persistentPrices);
 
                 return null;
             }
 
             private PersistentPurchase fetchPurchase(PersistenceManager persistenceManager) {
+                long start = currentTimeMillis();
                 String fetchGroupName = "priceCalculation";
                 persistenceManager.getFetchGroup(PersistentPurchase.class, fetchGroupName).addMember("shop").addMember("purchasings");
                 persistenceManager.getFetchGroup(PersistentPurchasing.class, fetchGroupName).addMember("article");
@@ -109,12 +78,16 @@ public class PricesWorker extends HttpServlet {
                 } finally {
                     persistenceManager.getFetchPlan().removeGroup(fetchGroupName);
                 }
+                if (LOG.isLoggable(Level.INFO)) {
+                    LOG.info((currentTimeMillis() - start) + "ms to fetch " + purchase);
+                }
 
                 return purchase;
             }
 
             @SuppressWarnings("unchecked")
             private PersistentPrices fetchPrices(PersistenceManager persistenceManager, PersistentShop shop) {
+                long start = currentTimeMillis();
                 Key key = KeyFactory.createKey(PersistentPrices.class.getSimpleName(), shop.getKeyOrThrow());
                 String fetchGroupName = "priceWithFields";
                 persistenceManager.getFetchGroup(PersistentPrices.class, fetchGroupName).addMember("shop").addMember("prices");
@@ -132,8 +105,57 @@ public class PricesWorker extends HttpServlet {
                     persistenceManager.getFetchPlan().removeGroup(fetchGroupName);
                     ServerTools.rollback(persistenceManager);
                 }
+                if (LOG.isLoggable(Level.INFO)) {
+                    LOG.info((currentTimeMillis() - start) + "ms to fetch prices of " + shop);
+                }
 
                 return persistentPrices;
+            }
+
+            private void updatePrices(PersistenceManager persistenceManager, PersistentPurchase purchase, PersistentPrices persistentPrices) {
+                PersistentShop shop = purchase.getShop();
+                Date purchaseDate = purchase.getPurchaseDate();
+                long start = currentTimeMillis();
+
+                Transaction tx = startTransaction(persistenceManager);
+                for (PersistentPurchasing purchasing : purchase) {
+                    BigDecimal quantity = purchasing.getQuantity();
+                    if (quantity == null) {
+                        continue;
+                    }
+
+                    PersistentArticle article = purchasing.getArticle();
+                    BigDecimal price = purchasing.getPrice().divide(quantity);
+                    PersistentPrice existingPrice = persistentPrices.getPriceFor(article);
+                    if (existingPrice == null) {
+                        persistentPrices.addPriceFor(article, purchaseDate, price);
+                        if (LOG.isLoggable(Level.INFO)) {
+                            LOG.info("Storing new price for " + shop + "/" + article);
+                        }
+                        continue;
+                    }
+
+                    boolean priceHasNotChanged = existingPrice.getPrice().equals(price);
+                    boolean existingPriceIsNewerThanNewPrice = existingPrice.getSince().compareTo(purchaseDate) > 0;
+                    if (priceHasNotChanged || existingPriceIsNewerThanNewPrice) {
+                        if (LOG.isLoggable(Level.FINE)) {
+                            LOG.fine("Keeping existing price for " + shop + "/" + article);
+                        }
+                        continue;
+                    }
+
+                    existingPrice.setSince((Date) purchaseDate.clone());
+                    existingPrice.setPrice(price);
+                    if (LOG.isLoggable(Level.INFO)) {
+                        LOG.info("Updating existing price for " + shop + "/" + article);
+                    }
+                }
+                persistenceManager.makePersistent(persistentPrices);
+                tx.commit();
+
+                if (LOG.isLoggable(Level.INFO)) {
+                    LOG.info((currentTimeMillis() - start) + "ms to update prices of " + shop);
+                }
             }
 
         });
