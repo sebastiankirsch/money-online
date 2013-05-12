af9ccddb27d2c03d1607a2c5b7cc188ab6893347
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-05-12 21:58:34 +0200

    javadoc added to mark interface ShoppingService as being used

--- src/main/java/net/tcc/money/online/client/ShoppingServiceAsync.java
+++ src/main/java/net/tcc/money/online/client/ShoppingServiceAsync.java
@@ -15,25 +15,36 @@ import java.util.Map;
 
 public interface ShoppingServiceAsync {
 
+    /** Call to {@link ShoppingService#loadShops()}. */
 	void loadShops(AsyncCallback<Iterable<Shop>> callback);
 
+    /** Call to {@link ShoppingService#createShop(String)}. */
 	void createShop(String name, AsyncCallback<Shop> callback);
 
+    /** Call to {@link ShoppingService#loadCategories()}. */
 	void loadCategories(AsyncCallback<Iterable<Category>> callback);
 
+    /** Call to {@link ShoppingService#createPurchase(net.tcc.money.online.shared.dto.Purchase)}. */
 	void createPurchase(Purchase purchase, AsyncCallback<Void> callback);
 
+    /** Call to {@link ShoppingService#loadArticles()}. */
 	void loadArticles(AsyncCallback<Iterable<Article>> asyncCallback);
 
+    /** Call to {@link ShoppingService#createCategory(String, net.tcc.money.online.shared.dto.Category)}. */
 	void createCategory(String name, Category selectedCategory, AsyncCallback<Category> callback);
 
+    /** Call to {@link ShoppingService#setParentFor(net.tcc.money.online.shared.dto.Category, net.tcc.money.online.shared.dto.Category)}. */
 	void setParentFor(Category category, Category parent, AsyncCallback<Category> asyncCallback);
 
+    /** Call to {@link ShoppingService#setCategoryFor(net.tcc.money.online.shared.dto.Article, net.tcc.money.online.shared.dto.Category)}. */
 	void setCategoryFor(Article article, Category category, AsyncCallback<Article> asyncCallback);
 
+    /** Call to {@link ShoppingService#loadPurchases(com.google.gwt.view.client.Range, String, boolean)}. */
     void loadPurchases(Range range, String columnName, boolean ascending, AsyncCallback<List<Purchase>> asyncCallback);
 
+    /** Call to {@link ShoppingService#loadCategorySpendings()}. */
     void loadCategorySpendings(AsyncCallback<Map<Category, BigDecimal>> asyncCallback);
 
+    /** Call to {@link ShoppingService#loadMonthlyCategorySpendings()}. */
     void loadMonthlyCategorySpendings(AsyncCallback<MonthlyExpensesData> asyncCallback);
 }
